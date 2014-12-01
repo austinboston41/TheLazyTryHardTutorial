af3d9ad9dc9520d1b557960bd2e80f9ce614423e
Daniel Parkin <delta63@live.ca>
2014-12-01 12:10:39 -0500

    2

--- app/src/main/java/com/example/austin/thelazytryhardtutorial/MyActivity.java
+++ app/src/main/java/com/example/austin/thelazytryhardtutorial/MyActivity.java
@@ -4,14 +4,36 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MyActivity extends Activity {
+
+    EditText username, password;
+    Button butSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        butSubmit = (Button) findViewById(R.id.butSubmit);
+
+
+        butSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = username.getText() + " , " + username.getText();
+                Toast toast = Toast.makeText(MyActivity.this, s,10);
+                toast.show();
+            }
+        });
     }
 
 
